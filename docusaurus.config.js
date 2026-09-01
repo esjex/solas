2f7ee679fadf0d64242b09e360d4e2965ba15629
Siobhan Fitzgerald-Gibson <siobhang64@gmail.com>
2026-09-01 14:21:27 +0100

    Updated favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Siobhan Fitzgerald-Gibson',
   tagline: 'Technical Author, UX Writer, Content Strategist',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/icons8-information-64.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
