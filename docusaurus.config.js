b42eb042e91cb29d1c4d63f4d310e680ec137053
Siobhan Fitzgerald-Gibson <siobhang64@gmail.com>
2026-09-16 17:50:50 +0100

    removed editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,8 +48,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-    'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         /* blog: {
           showReadingTime: true,
